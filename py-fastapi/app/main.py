28385dee9b0bf4328846f98df1048c761325afff
reynoldputra17 <reynoldputra1@gmail.com>
2023-03-26 20:42:38 +0700

    fix: setup guvicorn

--- py-fastapi/app/main.py
+++ py-fastapi/app/main.py
@@ -14,20 +14,42 @@ def hello_world():
 
 @app.post("/masuk")
 async def masuk_gate(request: GateRequest):
-    cursor.execute("SELECT is_aktif FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
-    result = cursor.fetchone()
-    if(result[0] == 0):
-        cursor.execute("UPDATE kartu_akses SET is_aktif = 1 WHERE id_kartu_akses = %s", (request.idkartu,))
+    cursor.execute("SELECT * FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
+    kartu = cursor.fetchone()
+    cursor.execute("SELECT * FROM register_gate WHERE id_register_gate = %d", (request.idgate,))
+    gate = cursor.fetchone()
+    if kartu is not None and gate is not None:
         return {"message" : "Berhasil memasuki gerbang"}
     else:
-        return {"message" : "Kartu sedang aktif"}
+        return {"message" : "Gagal memasuki gerbang"}
 
 @app.post("/keluar")
 async def keluar_gate(request: GateRequest):
-    cursor.execute("SELECT is_aktif FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
-    result = cursor.fetchone()
-    if(result[0] == 0):
-        cursor.execute("UPDATE kartu_akses SET is_aktif = 0 WHERE id_kartu_akses = %s", (request.idkartu,))
+    cursor.execute("SELECT * FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
+    kartu = cursor.fetchone()
+    cursor.execute("SELECT * FROM register_gate WHERE id_register_gate = %d", (request.idgate,))
+    gate = cursor.fetchone()
+    if kartu is not None and gate is not None:
         return {"message" : "Berhasil keluar gerbang"}
     else:
-        return {"message" : "Kartu sedang tidak aktif"}
+        return {"message" : "Gagal keluar gerbang"}
+
+# @app.post("/masuk")
+# async def masuk_gate(request: GateRequest):
+#     cursor.execute("SELECT is_aktif FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
+#     result = cursor.fetchone()
+#     if(result[0] == 0):
+#         cursor.execute("UPDATE kartu_akses SET is_aktif = 1 WHERE id_kartu_akses = %s", (request.idkartu,))
+#         return {"message" : "Berhasil memasuki gerbang"}
+#     else:
+#         return {"message" : "Kartu sedang aktif"}
+
+# @app.post("/keluar")
+# async def keluar_gate(request: GateRequest):
+#     cursor.execute("SELECT is_aktif FROM kartu_akses WHERE id_kartu_akses = %s", (request.idkartu,))
+#     result = cursor.fetchone()
+#     if(result[0] == 0):
+#         cursor.execute("UPDATE kartu_akses SET is_aktif = 0 WHERE id_kartu_akses = %s", (request.idkartu,))
+#         return {"message" : "Berhasil keluar gerbang"}
+#     else:
+#         return {"message" : "Kartu sedang tidak aktif"}
